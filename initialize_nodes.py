c431e4e4a30cc4e08ae9a5e5b7d3731b4c558e6f
Patrik Grman <pato.grman@gmail.com>
2020-05-02 15:50:47 +0200

    Clean code to eliminate all inspection complaints

--- initialize_nodes.py
+++ initialize_nodes.py
@@ -1,31 +1,32 @@
-from http.server  import BaseHTTPRequestHandler, HTTPServer
+from http.server import HTTPServer
 from node import get_handler
 from threading import Thread
 import time
 import requests
 
 
-#The condition argument is for you to know when everithing is running
-def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
-    servers = list(HTTPServer((HOST, port), get_handler()) for port in nodes)
+# The condition argument is for you to know when everything is running
+def do_stuff(host, nodes, edges, condition_ready=None, condition_done=None):
+    servers = list(HTTPServer((host, port), get_handler()) for port in nodes)
 
-    try:   
-        threads = list(Thread(target=server.serve_forever) for server in servers)
-        for t in threads: t.start()
+    threads = list(Thread(target=server.serve_forever) for server in servers)
+    try:
+        for t in threads:
+            t.start()
 
-        def add(x,y):
-            r = requests.get(f'http://{HOST}:{x}/new?port={y}')
-            r = requests.get(f'http://{HOST}:{y}/new?port={x}')
+        def add(x, y):
+            _ = requests.get(f'http://{host}:{x}/new?port={y}')
+            _ = requests.get(f'http://{host}:{y}/new?port={x}')
 
-        for x,y in edges:
-            add(x,y)
-            add(y, x)
-    
-        #This is here for you so you know when stuff is ready
+        for v0, v1 in edges:
+            add(v0, v1)
+            add(v1, v0)
+
+        # This is here for you so you know when stuff is ready
         if condition_ready is not None:
             with condition_ready:
                 condition_ready.notify()
-        
+
         if condition_done:
             with condition_done:
                 condition_done.wait()
@@ -39,14 +40,14 @@ def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
     for server in servers:
         server.shutdown()
         server.server_close()
-    for t in threads: t.join()
+    for t in threads:
+        t.join()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     HOST = "localhost"
     graph_base = 8030
-    graph = {(0,1), (1, 2), (1, 3), (1, 4), (3, 4), (4, 5)}
-    graph = {(graph_base+x, graph_base+y) for x,y in graph}
-    nodes = {x for y in graph for x in y}
-    do_stuff(HOST, nodes, graph)
-
+    graph = {(0, 1), (1, 2), (1, 3), (1, 4), (3, 4), (4, 5)}
+    graph = {(graph_base + x, graph_base + y) for x, y in graph}
+    _nodes = {x for y in graph for x in y}
+    do_stuff(HOST, _nodes, graph)
--- node.py
+++ node.py
@@ -1,42 +1,38 @@
-from urllib.parse  import urlparse, parse_qs
-from http.server  import BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+from http.server import BaseHTTPRequestHandler
 
 
 def get_handler():
     neighbours = set()
+
     class MyHandler(BaseHTTPRequestHandler):
         def _set_headers(self):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
+        # noinspection PyPep8Naming
         def do_GET(self):
             nonlocal neighbours
             self._set_headers()
             response = 'http://localhost:8000/ or http://localhost:8000/new?port=8080'
 
             parsed = urlparse(self.path)
-            pquery = parse_qs(parsed.query)
-            self._root = '..'
+            parsed_query = parse_qs(parsed.query)
             if parsed.path == '/':
                 response = ','.join(neighbours)
             if parsed.path == '/new':
-                self._name = pquery.get('port', (None,))[0]
-                if self._name is not None:
-                    neighbours.add(self._name)
+                name = parsed_query.get('port', (None,))[0]
+                if name is not None:
+                    neighbours.add(name)
                     response = 'Added or exists.'
                 else:
                     response = 'Nothing to add.'
 
             self.wfile.write(bytes(response, "UTF-8"))
-    
+
+        # noinspection PyPep8Naming
         def do_HEAD(self):
             self._set_headers()
 
     return MyHandler
-
-
-
-
-
-
